44f9a0b8aca8ab5824605c876224e9a1f79e5d6c
Musheer Ahmad Khan <103946554+Musheermindfire@users.noreply.github.com>
2023-04-20 01:43:33 +0530

    Added driversetup and log4j

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,42 +1,75 @@
 package base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 public class TestBase {
-    /*
-    private static ThreadLocal<ExtentReports> extent = new ThreadLocal<>();
-    public static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    @BeforeSuite
+    @BeforeSuite(alwaysRun = true)
     public void beforeSuite() {
-        ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
-        ExtentReports extentReports = new ExtentReports();
-        extentReports.attachReporter(spark);
-        extent.set(new Threadlocal)
-        extent.set(extentReports);
+        //This code block runs once before starting to suite execution
+
+        //This block can be used to perform activities like testrun creation
+        //in third party tools, parse secrets, creation of test data
+
     }
 
-    @BeforeMethod
-    public void beforeMethod(ITestResult result) {
-        ExtentTest extentTest = extent.get().createTest(result.getMethod().getMethodName());
-        test.set(new ThreadLocal<ExtentTest>());
-        test.get().set(extentTest);
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
+        /*
+         * This code block runs before starting of method execution
+         * As mostly test are executed on local/cloud linux server
+         * it is important to keep WebDriver instance headless in that case
+         */
+
+        /*
+         * //Setup code for chrome
+         * ChromeOptions chromeOptions = new ChromeOptions();
+         * chromeOptions.addArguments("--headless");
+         * driver.set(new ChromeDriver(chromeOptions));
+         */
+    
+        /*
+         * //Setup code for firefox
+         * FirefoxOptions firefoxOptions = new FirefoxOptions();
+         * firefoxOptions.addArguments("--headless");
+         * driver.set(new FirefoxDriver(firefoxOptions));
+         */
+
+        /*
+         * //Setup code for edge
+         * EdgeOptions edgeOptions = new EdgeOptions();
+         * edgeOptions.addArguments("--headless");
+         * driver.set(new EdgeDriver(edgeOptions));
+         */
+        
+        driver.get().manage().window().setSize(new Dimension(1920, 1080));
+        driver.get().manage().deleteAllCookies();
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void afterMethod(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE) {
-            test.get().fail(result.getThrowable());
-        } else if (result.getStatus() == ITestResult.SKIP) {
-            test.get().skip(result.getThrowable());
-        } else {
-            test.get().pass("Test passed");
-        }
-        extent.get().flush();
+        //This code block runs after completion of method execution
+        
+        //Teardown driver instance
+        driver.get().close();
+        driver.get().quit();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void afterSuite() {
-        extent.get().flush();
-    }*/
+        /*
+        This code block runs after completion of suite execution
+        and can be used to kill process, publish reports
+        */
+    }
 }
